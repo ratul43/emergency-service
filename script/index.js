7e58be209026866851019e8c126de1a6eb2f6faa
ratul43 <kmratul971@gmail.com>
2025-08-27 22:54:47 +0600

    call button all feature done

--- script/index.js
+++ script/index.js
@@ -1,3 +1,11 @@
+// function for fetching element by id
+function getElement(id){
+    const result = document.getElementById(id)
+    return result;
+}
+
+
+
 // Clicking on the 💗 heart icon of any card will increase the count in the Navbar
 
 document.getElementById("card-section")
@@ -22,15 +30,29 @@ document.getElementById("card-section")
 const serviceName = e.target.parentNode.parentNode.children[1].children[0].innerText
 const serviceNumber = e.target.parentNode.parentNode.children[1].children[2].innerText
 
+const coinNumber = parseInt(getElement("coin-count").innerText)
+if(coinNumber<=0){
+    alert("Out of coins. Please recharge and try again")
+    return;
+}
+
 const textMessage = `
 You are going to call
 ${serviceName} 
 ${serviceNumber}
 `
-
 alert(
     textMessage
 )
+
+
+
+availableCoinNumber = Number(coinNumber) - 20;
+getElement("coin-count").innerText = availableCoinNumber
+
+
+
+
 
 
 
